158457b0374746d896dec4fb4177ea925dec6f71
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2023-09-02 02:01:17 +0800

    增加设置权限部分

--- module_files/customize.sh
+++ module_files/customize.sh
@@ -78,3 +78,11 @@ fi
 # replace directory /system/product/app/AnalyticsCore
 # 仅针对 Magisk
 # replace file /system/product/app/AnalyticsCore/AnalyticsCore.apk
+
+# 设置权限函数
+# set_perm_recursive  <目录>                   <所有者> <用户组> <目录权限> <文件权限> <上下文> (默认值是: u:object_r:system_file:s0)
+# set_perm_recursive  "$MODPATH"/system/lib       0       0       0755       0644
+# set_perm  <文件名>                           <所有者> <用户组> <文件权限>    <上下文> (默认值是: u:object_r:system_file:s0)
+# set_perm  "$MODPATH"/system/bin/app_process32   0      2000     0755       u:object_r:zygote_exec:s0
+# set_perm  "$MODPATH"/system/bin/dex2oat         0      2000     0755       u:object_r:dex2oat_exec:s0
+# set_perm  "$MODPATH"/system/lib/libart.so       0        0      0644
