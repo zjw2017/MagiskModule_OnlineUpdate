c58498375d446f0814a39f5e71141e64e4e602cf
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2024-02-14 14:40:30 +0800

    更新相关注解

--- module_files/customize.sh
+++ module_files/customize.sh
@@ -6,18 +6,18 @@ SKIPUNZIP=0
 # ZIPFILE (path): 当前模块的安装包文件
 # ARCH (string): 设备的 CPU 构架，有如下几种arm, arm64, x86, or x64
 # IS64BIT (bool): 是否是 64 位设备
-# API (int): 当前设备的 Android API 版本 (如: Android 13.0 上为 33)
+# API (int): 当前设备的 Android API 版本 (如: Android 14.0 上为 34)
 
 # For Magisk
-# MAGISK_VER (string): 当前安装的 Magisk 的版本字符串 (如: 26.1)
-# MAGISK_VER_CODE (int): 当前安装的 Magisk 的版本代码 (如: 26100)
+# MAGISK_VER (string): 当前安装的 Magisk 的版本字符串 (如: 27.0)
+# MAGISK_VER_CODE (int): 当前安装的 Magisk 的版本代码 (如: 27000)
 # BOOTMODE (bool): 如果模块被安装在 Magisk 应用程序中则值为true
 
 # For KernelSU
 # KSU (bool): 标记此脚本运行在 KernelSU 环境下，此变量的值将永远为 true，你可以通过它区分 Magisk。
-# KSU_VER (string): KernelSU 当前的版本名字 (如: v0.4.0)
-# KSU_VER_CODE (int): KernelSU 用户空间当前的版本号 (如: 10672)
-# KSU_KERNEL_VER_CODE (int): KernelSU 内核空间当前的版本号 (如: 10672)
+# KSU_VER (string): KernelSU 当前的版本名字 (如: v0.7.6)
+# KSU_VER_CODE (int): KernelSU 用户空间当前的版本号 (如: 11434)
+# KSU_KERNEL_VER_CODE (int): KernelSU 内核空间当前的版本号 (如: 11434)
 # BOOTMODE (bool): 此变量在 KernelSU 中永远为 true
 
 if [[ $KSU == true ]]; then
@@ -68,7 +68,7 @@ fi
 # 用法: 函数名 类型 地址
 # 函数名: replace 或 (推荐使用) remove
 # replace 功能: 替换掉系统的某个目录,使其成为空目录
-# replace 可用类型为 file (仅针对 Magisk ) 或 directory
+# replace 可用类型为 file 或 directory
 # remove 功能: 删掉系统原来目录某个文件或者文件夹
 # remove 可用类型为 file 或 directory
 # 地址: 你要替换的地址
@@ -76,7 +76,6 @@ fi
 # remove file /system/product/app/AnalyticsCore/AnalyticsCore.apk
 # remove directory /system/product/app/AnalyticsCore
 # replace directory /system/product/app/AnalyticsCore
-# 仅针对 Magisk
 # replace file /system/product/app/AnalyticsCore/AnalyticsCore.apk
 
 # 设置权限函数
